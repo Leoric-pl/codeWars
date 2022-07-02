9a6e1a0aaa38461553c662435f61c372afbd128b
Leoric-pl <56604806+Leoric-pl@users.noreply.github.com>
2022-07-02 21:40:07 +0200

    Update StopgninnipSMysdroW!.cpp

--- StopgninnipSMysdroW!.cpp
+++ StopgninnipSMysdroW!.cpp
@@ -1,4 +1,8 @@
+/*
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
 
+Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
+*/
 std::string reverse(std::string word)
   {std::string Nword="";for(int a=word.length()-1;a>=0;a--){Nword+=word[a];}return Nword;}
 std::string spinWords(const std::string &str)
