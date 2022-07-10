9c3d1e54b0eb5764f6cf742be6818e1d40047d02
Leoric-pl <56604806+Leoric-pl@users.noreply.github.com>
2022-07-10 10:17:49 +0200

    Create Give me a Diamond.js

---
+++ Give me a Diamond.js
@@ -0,0 +1,50 @@
+/*
+Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
+Task
+
+You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
+
+Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
+Examples
+
+A size 3 diamond:
+
+ *
+***
+ *
+
+...which would appear as a string of " *\n***\n *\n"
+
+A size 5 diamond:
+
+  *
+ ***
+*****
+ ***
+  *
+
+...that is:
+
+"  *\n ***\n*****\n ***\n  *\n"
+
+
+*/
+
+
+function diamond(n){
+  if(n%2==0 || n<1)return null
+  else{
+    diam=""
+    for(var i=0;i<(n+1)/2;i++)
+      {for(var j=0;j<(n-1)/2-i;j++)diam+=" "
+      for(var j=0;j<i*2+1;j++)diam+="*" 
+       diam+="\n"
+      }
+    for(var i=(n-1)/2;i>0;i--)
+      {for(var j=0;j<(n+1)/2-i;j++)diam+=" "
+      for(var j=0;j<i*2-1;j++)diam+="*" 
+       diam+="\n"
+      }
+    
+  return diam;}
+}
